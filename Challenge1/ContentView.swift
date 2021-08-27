bcb5b89acd1e190334e1b983225e7ea6cbcbbbd6
Quinn McHenry <quinn@jqm.us>
2021-08-27 09:56:14 -0400

    Add preview content examples and base UI

--- Challenge1/ContentView.swift
+++ Challenge1/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Next Wake Up Only")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+
+            Card {
+                VStack {
+                    HStack(spacing: 36) {
+                        Text("BEDTIME")
+                            .bold()
+                        Text("WAKE UP")
+                            .bold()
+                    }
+                    .foregroundColor(Color("secondary"))
+                    .font(.callout)
+                    .padding(.top)
+                    Circle()
+                        .scaledToFit()
+                        .foregroundColor(Color("dial"))
+                    .padding()
+                }
+            }
             .padding()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color("backgroundScene"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let scheme = ColorScheme.dark
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(scheme)
+            Image("example")
+                .resizable()
+                .scaledToFit()
+                .preferredColorScheme(scheme)
+        }
     }
 }
