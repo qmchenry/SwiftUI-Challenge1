858cb53b2d1494f536041d87431cb5f8ea5530d4
Quinn McHenry <quinn@jqm.us>
2021-08-26 23:02:33 -0400

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Quinn McHenry on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
